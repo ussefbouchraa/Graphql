3a623ab11037b5aa8184b1aef2e21eca0d393c39
ussefbouchraa <ussefbouchraa@github.com>
2025-11-22 06:03:52 +0100

    stating point of the project GraphQl

---
+++ graphql/src/auth/login.js
@@ -0,0 +1,16 @@
+
+
+
+document.querySelector(".form").addEventListener("submit", () => {
+const username = document.querySelector(".username").value
+const password = document.querySelector(".username").value
+
+console.log(btoa(password))
+
+})
+
+
+async () =>{
+
+    //  fetch "https://learn.zone01oujda.ma/api/auth/signin" 
+}   
