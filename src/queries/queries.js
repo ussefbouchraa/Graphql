bcd87b240863b82a0bc11a4626221e60da4a1a8a
ussefbouchraa <ussefbouchraa@github.com>
2025-11-26 18:21:03 +0100

    adding new query for xp

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -18,4 +18,23 @@
         }
     `;
 
-   
+        //    query {
+        //     user {
+        //         id
+        //         login
+        //         firstName
+        //         lastName
+        //         email
+        //     }
+        //     transaction(where: {
+        //       type: {_eq:"xp"},
+        //       _or:[
+        //       	{object: {type : {_eq:"project"}, name:{_nlike:"quest%"}}},
+        //         {object:{type:{_eq:"piscine"}}},
+        //         {object:{type:{_eq:"module"}}}
+        //     	]
+        //     }){
+        //         amount
+        //         type
+        //     }
+        // }
